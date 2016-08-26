8b0146ba23d787259b35d9335788f362d2a27c13
lildutchie99 <misterhamin@gmail.com>
2016-08-26 10:37:04 -0700

    works with basic stemming, blacklist and tf threshold

--- Extract.py
+++ Extract.py
@@ -1,48 +1,78 @@
 import requests
 from bs4 import BeautifulSoup, Comment
-import string
-import sys
 import mysql.connector
+import sys
+import time
+import string
+import math
 from operator import itemgetter
+from functional import compose
+import re
 
 class Extract:
 
     """
+    Debug only function for optimizing
+    """
+    def br(self):
+        print('Total time: %fs' % (time.clock()-self.starttime,))
+        sys.exit()
+
+    """
     Takes optional paramaters for what year range to use in ngram retrieval
     """
-    def __init__(self, ngram_year=2008, ngram_smooth=3):
+    def __init__(self):
         self.punctbl = str.maketrans(string.punctuation.replace("'", ''), " " * (len(string.punctuation)-1)) #remove all except apostrophe
-        self.ngram_year = ngram_year
-        self.ngram_smooth = ngram_smooth
+
+    """
+    Adapted from https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring
+    """
+    def longest_common_substring(self, s1, s2):
+        m = [[0] * (1 + len(s2)) for i in range(1 + len(s1))]
+        longest, x_longest = 0, 0
+        for x in range(1, 1 + len(s1)):
+            for y in range(1, 1 + len(s2)):
+                if s1[x - 1] == s2[y - 1]:
+                    m[x][y] = m[x - 1][y - 1] + 1
+                    if m[x][y] > longest:
+                        longest = m[x][y]
+                        x_longest = x
+                else:
+                    m[x][y] = 0
+        return (x_longest-longest, s2.find(s1[x_longest - longest: x_longest]), longest, s1[x_longest - longest: x_longest])
 
     """
     Generate a value representing a word's 'fitness' as a potential keyword, based on it's TF and IDF
     Will and should be changed
     """
-    def tfidfComp(self, tf, idf):
-        return tf/idf if idf is not None else 0 #temporary solution
+    def termFitness(self, term):
+        dat = self.dict[term]
+        return math.log(dat[0])/dat[1] if dat[1] is not None else dat[0]*1000000000 #temporary solution
 
     """
     Query SQL db for frequency of a word
     """
     def requestFreq(self, word, cur):
         try:
-            cur.execute('select freq from unigrams_%s%s where word="%s";' % (word[0], word[1] if len(word) > 1 and word[1] != "'" else "", word))
+            #use case-insensitive reference
+            cur.execute('select freq from unigrams_%s%s_ci where word="%s" limit 1;'
+                        % (word[0], word[1] if len(word) > 1 and word[1] not in string.punctuation else "", word))
             resp = cur.fetchall()
         except:
             return None
         if len(resp) == 0: #no results
             return None
         else:
-            return sum([item[0] for item in resp]) #count all spellings
+            return resp[0][0]
 
     """
     @:param url: url of site to be mined
-    @:param tfThresh: minimum mentions of a term for it to be elegible as a keyword
+    @:param params: dictionary of assorted parameters relating to filtering and ranking
     @:param sqlconn: a MySQL connection object for accessing term frequency data
     @:return a dict including title, description, a list of images, and keywords
     """
-    def mine(self, url, tfThresh, sqlconn):
+    def mine(self, url, params, sqlconn):
+
         data = {'title':'', 'desc':'', 'imgs':[], 'kw':[]}
 
         doc = BeautifulSoup(requests.get(url).content, 'html.parser').html
@@ -50,27 +80,75 @@ class Extract:
         #with open('Test.html') as file:
         #    doc = BeautifulSoup(file.read(), 'html.parser').html
 
+        #for testing optimization... (ignore time needed to load page)
+        self.starttime = time.clock()
+
         #find all relevant text, excluding comments and scripts
         doctext = ' '.join(doc.findAll(text=lambda text: not (isinstance(text, Comment) or text.parent.name=='script')))
 
         doctext = doctext.translate(self.punctbl) #strip punctuation
-        words = doctext.split()
+        words = doctext.lower().split()
 
         cur = sqlconn.cursor()
 
-        tfdict = {}
-        for word in words:
-            if word not in tfdict:
-                tfdict[word] = 1
-            else:
-                tfdict[word] += 1
+        dict = {} #all the info about a term - TF, IDF, etc.
+        self.dict = dict #for reference by other functions
 
-        idfdict = {word[0]: self.requestFreq(word[0], cur) for word in tfdict.items() if word[1] >= tfThresh}
-        print(idfdict)
-        terms = sorted(idfdict.keys(), key=lambda item: self.tfidfComp(tfdict[item], idfdict[item]), reverse=True) #sort descending by tf/idf ratio
+        #first count term frequency
+        for word in words:
+            if word not in dict:
+                dict[word] = [1]
+            else:
+                dict[word][0] += 1
+
+        #remove terms with a TF below tfCutoff
+        dict = {key:dict[key] for key in dict if dict[key][0] >= params['tfCutoff']}
+
+        #remove blacklisted words
+        for word in [w for w in dict.keys()]:
+            cur.execute("select word from blacklist where word=%s;", (word,))
+            r = cur.fetchall()
+            if r != []:
+                del dict[word]
+            if re.compile('\\d+').match(word):
+                del dict[word]
+
+        #combine words with same root
+        plural = re.compile(',?e?s,?')
+
+        keys = list(dict.keys())
+        group = [] #list of keys to group
+
+        for w1idx in range(len(keys)):
+            for w2idx in range(w1idx, len(keys)):
+                w1 = keys[w1idx]
+                w2 = keys[w2idx]
+                if w1 != w2:
+                    pos1, pos2, l, sub = self.longest_common_substring(w1, w2)
+                    if l < params['groupCutoff']:
+                        continue
+                    pre = [w1[:pos1], w2[:pos2]]
+                    suf = [w1[pos1+l:], w2[pos2+l:]]
+                    if plural.match(','.join(suf)) and pre==['','']:
+                        group.append((w1, w2))
+
+        for g in group:
+            merger = g[0] if dict[g[0]][0] > dict[g[1]][0] else g[1] #group into one with higher TF
+            mergee = g[0] if merger==g[1] else g[1]
+            dict[merger][0] += dict[mergee][0]
+            del dict[mergee]
+
+        for word in dict:
+            dict[word].append(self.requestFreq(word, cur))
+
+        print(sorted(dict.items(), key=compose(itemgetter(0), itemgetter(1)), reverse=True))
+
+        terms = sorted(dict.keys(), key=self.termFitness, reverse=True) #sort descending by "fitness" determined by self.tfidfComp
 
         print(len(terms))
         print(terms)
+
+        print("Total time: %fs" % (time.clock()-self.starttime,))
 
         return data
 
@@ -78,9 +156,11 @@ class Extract:
 config = {'user':'root',
           'password':'qwertyuiop',
           'host':'127.0.0.1',
-          'database':'ngrams',
+          'database':'adxeed',
           'use_pure':False}
 conn = mysql.connector.connect(**config)
 
 e = Extract()
-e.mine('https://adxeed.com/blog/view/Case+study%3A+How+cross-network+advertising+helps+improve+overall+ROI', 3, conn)
+params = {'tfCutoff': 4,
+          'groupCutoff': 3}
+e.mine('https://www.codingwithkids.com/#!/', params, conn)
--- LoadNgramDB.py
+++ LoadNgramDB.py
@@ -1,14 +1,23 @@
 import mysql.connector
 import re
-import sys
+import statistics as stats
+import string
 
 #Utility to load Google unigram data into MySQL database
 
 def load(conn, suffix):
     #--------------------PARAMS--------------------------------
     fname = 'C:\\Users\\Ben\\Downloads\\ngrams\\googlebooks-eng-all-1gram-20120701-' + suffix
+    tcounts_fname = "C:\\Users\\Ben\\Downloads\\ngrams\\googlebooks-eng-all-totalcounts-20120701.txt"
     years = range(2000, 2009)
     #----------------------------------------------------------
+
+    tcounts = {}
+    with open(tcounts_fname) as file:
+        for line in file.read().split():
+            items = line.split(',')
+            if int(items[0]) in years:
+                tcounts[int(items[0])] = int(items[1])
 
     cur = conn.cursor()
 
@@ -26,36 +35,39 @@ def load(conn, suffix):
     with open(fname, encoding='utf-8') as file:
         for line in file:
             items = line.split('\t')
-            if int(items[1]) in years:
-                word = items[0] if '_' not in items[0] else items[0][:items[0].find('_')]
+            year = int(items[1])
+            if year in years:
+                word = items[0]
                 if word not in words:
-                    words[word] = [int(items[2])]
+                    words[word] = [float(items[2])/tcounts[year]]
                 else:
-                    words[word].append(int(items[2]))
+                    words[word].append(float(items[2])/tcounts[year])
 
-    print('Finding mean frequency...')
+    print('Finding median frequency...')
 
-    #take mean frequency
+    #take median frequency across relevant years
     for word in words:
-        words[word] = sum(words[word]) / len(words[word])
+        words[word] = stats.median(words[word])
 
     print('Uploading file: %s...' % (fname,))
 
     commit = 0 #save changes every 50 entries
-    filter = re.compile("^[qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM']*$")
+    filter = re.compile("^[qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'.\\-]*$")
     for word in words:
         if len(word) > 30:
             continue #skip really long words
-        if not filter.match(word):
+        if not filter.match(word): #no punctuation
             continue
         try:
-            cur.execute('insert into unigrams_%s%s(word, freq) values("%s", %s);' % (suffix, word[1] if len(word) > 1 and word[1] != "'" else "", word, words[word]))
+            cur.execute('insert into unigrams_%s%s(word, freq) values("%s", %s);' % (suffix, word[1] if len(word) > 1 and word[1] not in string.punctuation else "", word, words[word]))
         except:
             print(word)
             print(words[word])
             raise
-        if commit >= 50:
+        if commit >= 200:
+            print('Commiting...')
             conn.commit()
+            print('Done.')
             commit = 0
         commit += 1
 
@@ -65,9 +77,28 @@ def load(conn, suffix):
 
     cur.close()
 
-conn = mysql.connector.connect(user='root', password='qwertyuiop', host='127.0.0.1', database='ngrams', use_pure=False)
+"""
+Creates case-insensitive versions of the unigram tables
+"""
+def make_ci_tables(conn):
+    cur = conn.cursor()
+    for c1 in range(ord('a'), ord('z')+1):
+        for c2 in range(ord('a'), ord('z')+1):
+            print('Making CI table %s%s' % (chr(c1),chr(c2)))
+            cur.execute('create table unigrams_%s%s_ci select word, sum(freq) as freq from unigrams_%s%s group by word;' % (chr(c1), chr(c2), chr(c1), chr(c2)))
+            conn.commit()
+    for c in range(ord('a'), ord('z')+1):
+            print('Making CI table %s' % (chr(c),))
+            cur.execute('create table unigrams_%s_ci select word, sum(freq) from unigrams_%s group by word;' % (chr(c), chr(c)))
+            conn.commit()
+    cur.close()
 
-for ch in range(ord('b'), ord('z')+1):
-    load(conn, chr(ch))
+#creds...
+conn = mysql.connector.connect(user='root', password='qwertyuiop', host='127.0.0.1', database='adxeed', use_pure=False)
+
+#for ch in range(ord('a'), ord('z')+1):
+#    load(conn, chr(ch))
+
+make_ci_tables(conn)
 
 conn.close()
